f38067edd2148506dd7963e1486228be8b06eb49
IanLiu2603 <ian.liu2603@gmail.com>
2024-10-15 17:02:46 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,16 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+//Title
+const title = document.createElement("h1");
+title.innerHTML = "Title";
+app.append(title);
+
+//Canvas
+const canvas = document.createElement("canvas");
+
+canvas.width = 256;
+canvas.height = 256; 
+
+app.append(canvas);
