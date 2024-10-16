c6662990db8858bca90c76239c2be9f0813d4401
IanLiu2603 <ian.liu2603@gmail.com>
2024-10-15 20:16:40 -0700

    Step 2 Complete
    Code for drawing from the provided documentation

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,57 @@ app.append(title);
 
 //Canvas
 const canvas = document.createElement("canvas");
+const context = canvas.getContext("2d");
 
 canvas.width = 256;
 canvas.height = 256; 
 
 app.append(canvas);
+
+//Drawing
+let isDrawing: boolean = false;
+let x:number = 0;
+let y:number = 0;
+
+canvas.addEventListener("mousedown", (e)=>{
+    x = e.offsetX;
+    y = e.offsetY;
+    isDrawing = true;
+})
+
+canvas.addEventListener("mousemove", (e)=>{
+    if (isDrawing) {
+        drawLine(context, x, y, e.offsetX, e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+      }
+})
+
+canvas.addEventListener("mouseup", (e)=>{
+    if (isDrawing) {
+        drawLine(context, x, y, e.offsetX, e.offsetY);
+        x = 0;
+        y = 0;
+        isDrawing = false;
+      }
+})
+
+function drawLine(context, x1, y1, x2, y2) {
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+  }
+
+//Clear Button
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+
+clearButton.addEventListener("click", ()=>{
+    context.reset();
+})
+
+app.append(clearButton);
